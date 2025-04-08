f1b852ae542e3bfb84986e654343b5e84182de5a
AnushkaHiremath13 <anushkahiremath13@gmail.com>
2025-04-08 12:16:50 +0530

    save

--- sim.java
+++ sim.java
@@ -3,6 +3,7 @@ package java;
 public class sim {
     public static void main(String[] args) {
         int a=12,b=10;
+  
         System.out.println("addition of a and b ="+(a+b));
         System.out.println("subtraction of a and b ="+(a-b));
     }
